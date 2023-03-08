d0b5f47d48024a16d39490f1ba3eb0a9a97234a9
Tulio Fractal <tulioteodoromanso@gmail.com>
2023-03-08 10:41:02 -0300

    Heritage and a correction in file '.rspec'

--- test_app/spec/models/customer_factories_spec.rb
+++ test_app/spec/models/customer_factories_spec.rb
@@ -7,6 +7,21 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to eq("Sr. Tulio Manso")
   end
 
+  it '#full_name - Overwriting Attributes' do
+    customer = create(:customer, vip: false, days_to_pay: 10) 
+    expect(customer.vip).to eq(false)
+  end
+
+  it 'Heritage' do
+    customer = create(:customer_vip) 
+    expect(customer.vip).to eq(true)
+  end
+
+  it 'Heritage' do
+    customer = create(:customer_default) 
+    expect(customer.vip).to eq(false)
+  end
+
   it '#full_name' do
     customer = create(:user) # or create(:customer) # look at spec/factories/customer.rb
     expect(customer.full_name).to start_with("Sr. ")
