c2a8508ccb8a8f25c782024521c89eb3f80ae855
Tulio Fractal <tulioteodoromanso@gmail.com>
2023-03-08 12:26:21 -0300

    Create List

--- test_app/spec/models/order_spec.rb
+++ test_app/spec/models/order_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Order, type: :model do
     order = create(:order)
     expect(order.customer).to be_kind_of(Customer)
   end
+
+  it 'Have 3 orders' do
+    orders = create_list(:order, 3) # we can orverwrite elemnts just by pass a attribute before the ',' ex: "create_list(:customer, 5, description: "Testttt")"
+    expect(orders.count).to eq(3)
+  end
 end
