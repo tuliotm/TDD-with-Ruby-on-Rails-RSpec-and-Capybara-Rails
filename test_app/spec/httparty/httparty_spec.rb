964b7567723d9010ab79a8912b1dfe04b4e7fae8
TulioTMWsl2 <tulioteodoromanso@gmail.com>
2023-03-08 18:01:53 -0300

    HTTParty, json

---
+++ test_app/spec/httparty/httparty_spec.rb
@@ -0,0 +1,8 @@
+describe 'HTTParty' do
+    it 'HTTParty' do
+        response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+        content_type = response.headers['content-type']
+        puts content_type
+        expect(content_type).to match(/application\/json/)
+    end
+end
