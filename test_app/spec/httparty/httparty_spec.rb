2ad17596c8f4eff1201a82100cd21121ce455df6
TulioTMWsl2 <tulioteodoromanso@gmail.com>
2023-03-08 18:35:52 -0300

    VCR

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,10 +1,12 @@
 describe 'HTTParty' do
-    it 'Content-type' do
-        stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
-        to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
-
+    it 'content-type' do
+      #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+      #     to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
+  
+      VCR.use_cassette("jsonplaceholder/posts") do
         response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
         content_type = response.headers['content-type']
         expect(content_type).to match(/application\/json/)
+      end
     end
-end
+  end
