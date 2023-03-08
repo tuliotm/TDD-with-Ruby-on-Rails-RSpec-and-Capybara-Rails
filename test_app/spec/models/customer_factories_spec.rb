9a73f7681b0a55ad80e9ddc67dff1eaae2e8ea94
Tulio Fractal <tulioteodoromanso@gmail.com>
2023-03-08 10:24:35 -0300

    Overwriting attributes and Aliases for factorys

--- test_app/spec/models/customer_factories_spec.rb
+++ test_app/spec/models/customer_factories_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
 
+  it '#full_name - Overwriting Attributes' do
+    customer = create(:customer, name: "Tulio Manso") # forcing the name that i want
+    expect(customer.full_name).to eq("Sr. Tulio Manso")
+  end
+
   it '#full_name' do
-    customer = create(:customer) # look at spec/factories/customer.rb
+    customer = create(:user) # or create(:customer) # look at spec/factories/customer.rb
     expect(customer.full_name).to start_with("Sr. ")
   end
 
