7ef82c1d1a4bfc8b656972b91f9349fea767f364
Tulio Fractal <tulioteodoromanso@gmail.com>
2023-03-08 09:53:35 -0300

    Starting with Fixtures

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :customers # we can use ":all" to load all fixtures
+
+  it 'Create a Customer' do
+    customers = customers(:tulio) # look spec/fixtures/customers.yml
+    expect(customers.full_name).to eq("Sr. Tulio Manso") 
+  end
 end
