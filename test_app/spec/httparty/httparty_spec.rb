4851fa4135966a6f21756e6e3c6c94d28a7ca460
TulioTMWsl2 <tulioteodoromanso@gmail.com>
2023-03-08 18:19:07 -0300

    Webmock

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,8 +1,10 @@
 describe 'HTTParty' do
-    it 'HTTParty' do
+    it 'Content-type' do
+        stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+        to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
+
         response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
         content_type = response.headers['content-type']
-        puts content_type
         expect(content_type).to match(/application\/json/)
     end
 end
