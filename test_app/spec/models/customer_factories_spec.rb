3cb8c48a2b666b741d51e4f73d87ac4db86991df
Tulio Fractal <tulioteodoromanso@gmail.com>
2023-03-09 09:29:02 -0300

    Time Helpers

--- test_app/spec/models/customer_factories_spec.rb
+++ test_app/spec/models/customer_factories_spec.rb
@@ -65,5 +65,14 @@ RSpec.describe Customer, type: :model do
     expect(customer.gender).to eq('F')
   end
 
+  it 'travel_to' do
+    travel_to Time.new(2004, 11, 24, 01, 04, 44) do
+      @customer = create(:customer_vip)
+    end
+
+    expect(@customer.created_at).to eq(Time.new(2004, 11, 24, 01, 04, 44))
+    expect(@customer.created_at).to be < Time.now
+  end
+
   it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
 end
--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -34,6 +34,9 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
 
+  # Time Helper
+  config.include ActiveSupport::Testing::TimeHelpers
+
   # FactoryBot
   config.include FactoryBot::Syntax::Methods
 
