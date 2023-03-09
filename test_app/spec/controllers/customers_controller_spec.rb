72403fa0359bff254eddc8491ecdd8327a70834e
Tulio Fractal <tulioteodoromanso@gmail.com>
2023-03-09 11:48:47 -0300

    Testing Controllers (with authentication)

--- test_app/spec/controllers/customers_controller_spec.rb
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -1,13 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe CustomersController, type: :controller do
-    it 'responds successfully' do
+
+  describe 'as a Guest' do
+    context '#index' do
+      it 'responds successfully' do
         get :index
         expect(response).to be_success
-    end
+      end
 
-    it 'responds a 200 response' do
+      it 'responds a 200 response' do
         get :index
         expect(response).to have_http_status(200)
+      end
     end
+
+    it 'responds a 302 response (not authorized)' do
+      customer = create(:customer)
+      get :show, params: { id: customer.id }
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe 'as Logged Member' do
+    it 'responds a 200 response' do
+      member = create(:member)
+      customer = create(:customer)
+
+      sign_in member
+
+      get :show, params: { id: customer.id }
+      expect(response).to have_http_status(200)
+    end
+
+    it 'render a :show template' do
+      member = create(:member)
+      customer = create(:customer)
+
+      sign_in member
+
+      get :show, params: { id: customer.id }
+      expect(response).to render_template(:show)
+    end
+  end
+
 end
