cfa0d91b0e223123cdbd9e6e6f3ab0a49e67cb06
Tulio Fractal <tulioteodoromanso@gmail.com>
2023-03-09 11:16:06 -0300

    Testing Controllers

---
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe CustomersController, type: :controller do
+    it 'responds successfully' do
+        get :index
+        expect(response).to be_success
+    end
+
+    it 'responds a 200 response' do
+        get :index
+        expect(response).to have_http_status(200)
+    end
+end
