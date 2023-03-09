0f92953087e8196ea7b3d48ca1da6ae0b8b8407f
Tulio Fractal <tulioteodoromanso@gmail.com>
2023-03-09 08:56:32 -0300

    All VCR's lessons remaining

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,12 +1,10 @@
 describe 'HTTParty' do
-    it 'content-type' do
-      #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
-      #     to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
-  
-      VCR.use_cassette("jsonplaceholder/posts") do
-        response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-        content_type = response.headers['content-type']
-        expect(content_type).to match(/application\/json/)
-      end
-    end
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+    #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+    #     to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
+
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1,2,3,4,5].sample}")
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
   end
+end
